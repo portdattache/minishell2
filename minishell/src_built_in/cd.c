23336677aa35894a17f2ac9fe45e322d5e845fa7
Baptiste Caumont <bcaumont@c1r7p6.42lehavre.fr>
2025-04-17 17:18:43 +0200

    error cd corrigee

--- minishell/src_built_in/cd.c
+++ minishell/src_built_in/cd.c
@@ -6,21 +6,44 @@
 /*   By: bcaumont <bcaumont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 08:59:27 by bcaumont          #+#    #+#             */
-/*   Updated: 2025/04/09 16:03:01 by bcaumont         ###   ########.fr       */
+/*   Updated: 2025/04/17 17:16:14 by bcaumont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+static int	count_args(char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
+}
+
 int	ft_cd(char **args, t_shell *shell)
 {
+	char	*path;
+
 	(void)shell;
-	if (!args[1])
+	if (count_args(args) > 2)
 	{
-		write(2, "cd: missing argument\n", 21);
+		write(2, "cd: too many arguments\n", 24);
 		return (1);
 	}
-	if (chdir(args[1]) == -1)
+	if (!args[1])
+	{
+		path = getenv("HOME");
+		if (!path)
+		{
+			write(2, "cd: HOME not set\n", 21);
+			return (1);
+		}
+	}
+	else
+		path = args[1];
+	if (chdir(path) == -1)
 	{
 		perror("cd");
 		return (1);
